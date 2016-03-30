f34a843bf3a5e1ab42737fd536d8beec1b095c4f
lcarneiro <lucas.rod.carneiro@gmail.com>
2016-03-30 21:57:24 +1000

    Updating gitignore; Adding a timer.

---
+++ Assets/Scripts/TimeBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeBar : MonoBehaviour {
+
+    public Text timeText;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+        time = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        //Time using format Minutes:Seconds:Milliseconds
+        timeText.text = "" + Mathf.FloorToInt(time / 60) + ":" + Mathf.Round(time) + ":" +  time.ToString("F2").Split('.')[1];
+    }
+}
